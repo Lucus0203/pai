dec7237faf99df36798f957dcedcf953ff9a997a
Lucus0203 <liting68@163.com>
2016-07-29 17:44:26 +0800

    testpai

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pai.trainingpie.com/';
-$config['web_url'] = 'http://wx.trainingpie.com/';
+$config['base_url'] = 'http://testpai.trainingpie.com';
+$config['web_url'] = 'http://testwx.trainingpie.com';
 
 /*
 |--------------------------------------------------------------------------
