538bd07cbf474dacf7f99e70ec7c10fb15577221
Lucus0203 <liting68@163.com>
2016-08-06 11:15:04 +0800

    zhidingsms

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -253,7 +253,7 @@ class Login extends CI_Controller
             $this->user_model->create(array('mobile' => $mobile, 'mobile_code' => $code, 'created' => date("Y-m-d H:i:s"), 'status' => 1));
         }
         $this->load->library('zhidingsms');
-        $this->zhidingsms->sendSMS($mobile, '您本次的验证码是:'.$code);
+        $this->zhidingsms->sendTPSMS($mobile,'@1@='.$code,'ZD30018-0001');
         echo 1;
     }
 
