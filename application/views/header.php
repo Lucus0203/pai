9133c05224da3a2f389d9133016143976f91c381
Lucus0203 <liting68@163.com>
2016-08-08 01:20:28 +0800

    css time

--- application/views/header.php
+++ application/views/header.php
@@ -5,20 +5,20 @@
 		<meta charset="UTF-8">
 		<title>培训派</title>
 		<link rel="icon" href="favicon.ico" type="image/x-icon" />
-		<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/common.css" />
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/common.css?0808" />
 
 		<script type="text/javascript" src="<?php echo base_url();?>js/jquery1.83.js"></script>
 		<script type="text/javascript" src="<?php echo base_url();?>js/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url();?>js/additional-methods.min.js"></script>
 		<script type="text/javascript"  src="<?php echo base_url() ?>js/wdate/WdatePicker.js"></script>
 <script type="text/javascript">
-//var _hmt = _hmt || [];
-//(function() {
-//  var hm = document.createElement("script");
-//  hm.src = "//hm.baidu.com/hm.js?9432a72cc245c2b9cafed658f471d489";
-//  var s = document.getElementsByTagName("script")[0];
-//  s.parentNode.insertBefore(hm, s);
-//})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?9432a72cc245c2b9cafed658f471d489";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
 			$(document).ready(function(){
 				$('.listBox').delegate('.listCont','hover',function(){
 					$(this).toggleClass('hover');
