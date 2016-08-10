1fcc1f92948ba4977a902127ee2a5e8a278f740c
Lucus0203 <liting68@163.com>
2016-08-10 15:59:42 +0800

    二级部门

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -86,7 +86,7 @@ class Student extends CI_Controller
 
         $where = "parent_id is null and company_code = '{$logininfo['company_code']}' ";
         $departments = $this->department_model->get_all($where);
-        $second_departments = $this->department_model->get_all(array('parent_id' => $departments[0]['id']));
+        $second_departments = (!empty($departments))?$this->department_model->get_all(array('parent_id' => $departments[0]['id'])):array();
         foreach ($departments as $key => $d) {
             if (($d['id'] == $current_department['parent_id']) || $d['id'] == $dpid) {
                 $departments[$key]['departs'] = $this->department_model->get_all(array('parent_id' => $d['id']));
