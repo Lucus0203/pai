11f95c307607ece63423617ff81d1fad3a69a5a7
Lucus0203 <liting68@163.com>
2016-09-01 00:07:41 +0800

    table css

--- application/views/course/apply_list.php
+++ application/views/course/apply_list.php
@@ -47,10 +47,10 @@
                         <col width="10%">
                         <col width="10%">
                         <col width="20%">
-                        <col width="20%">
+                        <col width="25%">
                         <col width="10%">
                         <col width="20%">
-                        <col width="20%">
+                        <col width="5%">
                         <col width="5%">
                         <col width="5%">
                     </colgroup>
