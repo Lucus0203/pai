cbd7286387f8f32b2fe7a0882117c5a226f1840d
Lucus0203 <liting68@163.com>
2016-08-31 08:22:10 +0800

    walkthrough

--- application/views/walkthrough.php
+++ application/views/walkthrough.php
@@ -8,28 +8,6 @@
 
             steps: [
                 {
-                    wrapper: '#naviDepartmentStudent',
-                    margin: '0',
-                    popup: {
-                        content: '#navi-department',
-                        type: 'tooltip',
-                        position: 'bottom',
-                        offsetHorizontal: 0,
-                        offsetVertical: 0,
-                        width: '500'
-                    }
-                }, {
-                    wrapper: '#naviTeacher',
-                    margin: '0',
-                    popup: {
-                        content: '#navi-teacher',
-                        type: 'tooltip',
-                        position: 'bottom',
-                        offsetHorizontal: 0,
-                        offsetVertical: 0,
-                        width: '500'
-                    }
-                }, {
                     wrapper: '#naviCourse',
                     margin: '0',
                     popup: {
@@ -79,27 +57,25 @@
 </script>
 <!-- walkthrough -->
 <div id="walkthrough">
-    <div id="navi-department" style="display:none;">
-        <p class="tooltipTitle">第一步：创建学员</p>
-        <p><img src="<?php echo base_url();?>images/walkthrough01.png" width="80%" /></p>
-        <p style="text-align: left;padding: 0 25px;">为您的课程创建第一个学员.</p>
-        <br>
-        <a href="javascript:;" class="next-step" style="float:right;">下一步</a>
-    </div>
-    <div id="navi-teacher" style="display:none;">
-        <p class="tooltipTitle">第二步：创建讲师</p>
-        <p><img src="<?php echo base_url();?>images/walkthrough02.png" width="80%" /></p>
-        <p style="text-align: left;padding: 0 25px;">谁来上课呢?</p>
-        <br>
-        <a href="javascript:;" class="prev-step" style="float:left;">上一步</a>
-        <a href="javascript:;" class="next-step" style="float:right;">下一步</a>
-    </div>
+<!--    <div id="navi-department" style="display:none;">-->
+<!--        <p class="tooltipTitle">第一步：创建学员</p>-->
+<!--        <p><img src="--><?php //echo base_url();?><!--images/walkthrough01.png" width="80%" /></p>-->
+<!--        <p style="text-align: left;padding: 0 25px;">为您的课程创建第一个学员.</p>-->
+<!--        <br>-->
+<!--        <a href="javascript:;" class="next-step" style="float:right;">下一步</a>-->
+<!--    </div>-->
+<!--    <div id="navi-teacher" style="display:none;">-->
+<!--        <p class="tooltipTitle">第二步：创建讲师</p>-->
+<!--        <p><img src="--><?php //echo base_url();?><!--images/walkthrough02.png" width="80%" /></p>-->
+<!--        <p style="text-align: left;padding: 0 25px;">谁来上课呢?</p>-->
+<!--        <br>-->
+<!--        <a href="javascript:;" class="prev-step" style="float:left;">上一步</a>-->
+<!--        <a href="javascript:;" class="next-step" style="float:right;">下一步</a>-->
+<!--    </div>-->
     <div id="navi-course" style="display:none;">
-        <p class="tooltipTitle">最后一步：创建课程</p>
+        <p class="tooltipTitle">第一步：创建课程</p>
         <p><img src="<?php echo base_url();?>images/walkthrough03.png" width="80%" /></p>
-        <p style="text-align: left;padding: 0 25px;">为您的课程添加一些信息.</p>
-        <br>
-        <a href="javascript:;" class="prev-step" style="float:left;">上一步</a>
+        <p style="text-align: center;padding: 0 25px;">您的高效培训管理,从创建第一个课程开始.</p>
         <a href="javascript:;" class="close-step" style="float:right;">清楚了</a>
     </div>
 </div>
