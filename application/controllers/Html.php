12d52f0ce14afb662fbbf950d58addeaa394f17c
Lucus0203 <liting68@163.com>
2016-09-08 22:02:08 +0800

    price

--- application/controllers/Html.php
+++ application/controllers/Html.php
@@ -164,7 +164,7 @@ class Html extends CI_Controller {
         $message.='<p>发送时间'. date("Y年m月d日 H:i:s").'</p>';
         $this->email->clear();
         $tomail = '419993435@qq.com';//'service@trainingpie.com';
-        $this->email->from($this->_logininfo['email'], $company['name']);
+        $this->email->from('service@trainingpie.com', $company['name']);//$this->_logininfo['email']
         $this->email->subject("{$company['name']} ".$this->_logininfo['real_name']."的价格清单");
         $this->email->to($tomail);//
         $this->email->message($message);
