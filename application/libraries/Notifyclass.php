a5efb06e0af440e393797f782ce29f8de76687f6
Lucus0203 <liting68@163.com>
2016-11-24 12:55:50 +0800

    update notify

--- application/libraries/Notifyclass.php
+++ application/libraries/Notifyclass.php
@@ -154,7 +154,6 @@ EOF;
                         )
                     );
                     if($objTempid->errcode=='0') {
-                        print_r($objTempid);
                         $templateid = $objTempid->template_id;
                         $res = $this->CI->wechat->templateSend($student['openid'], $templateid, $link, $wxdata);
                     }
